2e863fdbf855a3120912ec2854ccd7e055514a91
agarwalshivam168 <agarwalshivam168@gmail.com>
2021-01-12 17:06:49 +0530

    Commit

--- channel.php
+++ channel.php
@@ -3,17 +3,14 @@
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://api.bigcommerce.com/stores/ausuz9ld5n/v3/carts",
+  CURLOPT_URL => "https://api.bigcommerce.com/stores/ausuz9ld5n/v3/channels",
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => "",
   CURLOPT_MAXREDIRS => 10,
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "{\"line_items\":[{\"quantity\":5,\"product_id\":81}]}",
+  CURLOPT_CUSTOMREQUEST => "GET",
   CURLOPT_HTTPHEADER => array(
-    "accept: application/json",
-    "content-type: application/json",
     "x-auth-token: e96kxrwyt46roo02s8v111sekdeecq8"
   ),
 ));
